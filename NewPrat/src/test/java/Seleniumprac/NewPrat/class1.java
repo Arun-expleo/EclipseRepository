dcc5a34a77a31adcede9367baee7ebd676b7329b
Mahesh Raydu <PlaywrightLearn@outlook.com>
2026-06-17 12:46:36 +0530

    commit

--- NewPrat/src/test/java/Seleniumprac/NewPrat/class1.java
+++ NewPrat/src/test/java/Seleniumprac/NewPrat/class1.java
@@ -4,7 +4,7 @@ public class class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Guys");
-
+//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
 	}
 
 }
